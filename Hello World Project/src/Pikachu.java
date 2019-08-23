1bf2464ce5751f5333aa2a127ab329c1bf7b47fa
APCS3rd <APCS3rd@MHS-LT9-31>
2019-08-23 10:56:39 -0700

    PIKACHU	PIKA PI

---
+++ Hello World Project/src/Pikachu.java
@@ -0,0 +1,12 @@
+
+public class Pikachu {
+
+	public static void main(String[] args) {
+		System.out.println("Pikachu welcomes you to the World of Pokemon!");
+		System.out.println("\t\t\t  (\\__/)");
+		System.out.println("\t\t\t  (o^.^)");
+		System.out.println("\t\t\tz(_(\")(\"))");
+
+	}
+
+}
