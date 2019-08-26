ab63dca20247a7bc1de172acffbd946eb04bddbd
APCS3rd <APCS3rd@MHS-LT9-31>
2019-08-26 10:18:30 -0700

    new deal for the pikachu

--- Hello World Project/src/Pikachu.java
+++ Hello World Project/src/Pikachu.java
@@ -1,7 +1,11 @@
+/* 3rd Period APCS Class Composite
+ * 8-26-19
+ * Hans Mercado 
+ * Draws a Pikachu using escape sequences.
+ */
+public class Pikachu { //class header
 
-public class Pikachu {
-
-	public static void main(String[] args) {
+	public static void main(String[] args) { //main method
 		System.out.println("Pikachu welcomes you to the World of Pokemon!");
 		System.out.println("\t\t\t  (\\__/)");
 		System.out.println("\t\t\t  (o^.^)");
