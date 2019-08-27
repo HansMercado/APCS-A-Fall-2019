b094f8fff21f25ae209f5cf9cb24ca59ab5d0184
APCS3rd <APCS3rd@MHS-LT9-31>
2019-08-27 11:08:02 -0700

    8-27-19, box thing now

--- Hello World Project/src/Pikachu.java
+++ Hello World Project/src/Pikachu.java
@@ -5,12 +5,35 @@
  */
 public class Pikachu { //class header
 
-	public static void main(String[] args) { //main method
+	public static void main(String[] args) {
+		printPikachu();
+		drawBox();
+		System.out.println();
+		drawBox();
+		System.out.println();
+		drawBox();
+		System.out.println();
+	}
+	public static void printPikachu() {
+
 		System.out.println("Pikachu welcomes you to the World of Pokemon!");
 		System.out.println("\t\t\t  (\\__/)");
 		System.out.println("\t\t\t  (o^.^)");
 		System.out.println("\t\t\tz(_(\")(\"))");
-
 	}
-
+	public static void plusMinusPlus() {
+		System.out.println("+----------+");
+		
+	}
+	public static void twoVerticalLines() {
+		System.out.println("|          |");
+	}
+	public static void drawBox() {
+		plusMinusPlus();
+		twoVerticalLines();
+		twoVerticalLines();
+		twoVerticalLines();
+		twoVerticalLines();
+		plusMinusPlus();
+	}
 }
