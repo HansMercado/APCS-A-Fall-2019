5d26b19e6e196e98d4399af820c2f5e39c18f23d
APCS3rd <APCS3rd@MHS-LT9-31>
2019-09-10 11:00:23 -0700

    did like the entirety of part 2, started part 3 finished exponent

--- Access Calculate/src/DoMath.java
+++ Access Calculate/src/DoMath.java
@@ -9,14 +9,27 @@ public class DoMath {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 			System.out.println("yeehaw");
-			System.out.println(Calculate.square(9));
-			System.out.println(Calculate.cube(2));
-			System.out.println(Calculate.average(3, 2));
+			System.out.println("square :"+Calculate.square(9));
+			System.out.println("cube :"+Calculate.cube(2));
+			System.out.println("average :"+Calculate.average(3, 2));
 			System.out.println(Calculate.average(3, 1, 2));
 			System.out.println(Calculate.toDegrees(3.14159));
 			System.out.println(Calculate.toRadians(360));
 			System.out.println(Calculate.discriminant(1, -8, 16));
 			System.out.println(Calculate.toImproperFrac(1, 2, 9));
+			System.out.println(Calculate.toMixedNum(15, 2));
+			System.out.println(Calculate.foil(2, 3, 6, -7, "g(x)"));
+			System.out.println(Calculate.foil(1, 0, 1, 0, "h(x)")); //might come back and get rid of the 0 stuff later
+			//start of part 2 ********************************
+			System.out.println(Calculate.isDivsibleBy(72, 91));
+			System.out.println(Calculate.absValue(-9000.1));
+			System.out.println(Calculate.max(2, 3));
+			System.out.println(Calculate.max(2, 3, 3));
+			System.out.println(Calculate.min(700, 701));
+			System.out.println(Calculate.round2(.554));
+			System.out.println(Calculate.round2(.555));
+			//start of part 3 ********************************
+			System.out.println(Calculate.exponent(2, 10));
 			
 	}
 }
--- Calculate Library/Calculate.java
+++ Calculate Library/Calculate.java
@@ -28,7 +28,67 @@ public class Calculate {
 		return ((B*B)-4*A*C); //B^2-4ac is the discriminant lol I wish I had paid attention and learned this when it was more relevant
 	}
 	public static String toImproperFrac(int whole, int numerator, int denominator) {
-		return (whole*denominator+numerator+"/"+denominator);
+		return (whole*denominator+numerator+"/"+denominator); //gets the whole number and multiplies it by the denominator, then adds it to the numerator. then adds the "/" and the denominator.
 	}
-	public static String toMixedNum(int numerator, int denominator)
+	public static String toMixedNum(int numerator, int denominator) {
+		return (numerator/denominator+"_"+numerator%denominator+"/"+denominator); //"numerator/denominator" returns the whole number, while "numerator%denominator" gets our remainder the rest of the return statement just includes the chars needed by the instructions.
+	}
+	public static String foil(int A, int B, int C, int D, String var) { //(ax+b)(cx+d) form. A and C are coefficients, B and D are the constants. Variable "var" replaces x.
+		int finalA = A*C; //the "a" in ax^2+bx+c.
+		int finalB = (A*D)+(C*B); //the "b" in ax^2+bx+c.
+		int finalC = D*B; //the "c" in ax^2+bx+c
+		return (finalA+var+"^2"+" + "+finalB+var+" + "+finalC); //puts em together or should i say CONCATENATES THEM
+	}
+	//start of part 2 ************************************************
+	public static boolean isDivsibleBy(int dividend, int divisor) {
+		if (dividend%divisor==0) { //dividend is either evenly divisible by the divisor or it ain't
+			return(true);
+		}else {
+			return(false);
+		}
+	}
+	public static double absValue(double num) {
+		if (num<0) { //eliminate negative numbers
+			num=num*-1;
+		}
+		return(num);
+		}
+	public static double max(double num1, double num2) {
+		if (num1>num2) { //returns the greatest input value. if they're equal, the maximum is still decided.
+			return(num1);
+		}else {
+			return(num2);
+		}	
+	}
+	public static double max(double num1, double num2, double num3) {
+		if (num1>=num2&&num1>=num3) {
+			return(num1);
+		}else if (num2>=num1&&num2>=num3) {
+			return(num2);
+		}else if (num3>=num1&&num3>=num2) {
+			return(num3);
+		}else {
+			return(num1);
+		}
+	}
+	public static double min(double num1, double num2) {
+		if (num1<num2) {
+			return(num1);
+		}else {
+			return(num2);
+		}
+	}
+	public static double round2(double num1) { //visualize 0.555 and 0.554
+		num1=((num1+.005)*100);
+		num1 = (int) num1;
+		return(num1/100);
+	}
+	//start of part3 ****************************************************
+	public static double exponent(double num, int power) {
+		double ans = num;
+		for (int i=1;i!=power;i++) {
+			ans=(ans*num);
+		}
+		return(ans);
+	}
 }
