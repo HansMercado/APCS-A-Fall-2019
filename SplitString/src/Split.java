9e6c995a42ea4778f2b68f9c9b459621d1e6f79e
APCS3rd <APCS3rd@MHS-LT9-31>
2019-10-21 10:56:32 -0700

    IT BEGINS

---
+++ SplitString/src/Split.java
@@ -0,0 +1,20 @@
+/* Hans Mercado 
+ * 21-10-19
+ * Dreyer 3rd Period
+ * 
+ */
+import java.util.Arrays;
+public class Split {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String[] outputArray = "I like apples!".split(" ");
+		System.out.println(Arrays.toString(outputArray));
+		String[] outputArray2 = " really I really like really red apples!".split(" really ");
+		System.out.println(Arrays.toString(outputArray2));
+		String[] part1split = "applespineapplesbreadlettucetomatobaconmayohambreadcheese".split("bread");
+		part1split = Arrays.toString(part1split).split("mayo");
+		
+	}
+
+}
