21451dfc4563d1f272aa17b010970be32bab7704
APCS3rd <APCS3rd@MHS-LT9-31>
2019-10-22 11:06:50 -0700

    okay then

--- SplitString/src/Split.java
+++ SplitString/src/Split.java
@@ -8,12 +8,16 @@ public class Split {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String[] outputArray = "I like apples!".split(" ");
+		/*/String[] outputArray = "I like apples!".split(" ");
 		System.out.println(Arrays.toString(outputArray));
 		String[] outputArray2 = " really I really like really red apples!".split(" really ");
 		System.out.println(Arrays.toString(outputArray2));
-		String[] part1split = "applespineapplesbreadlettucetomatobaconmayohambreadcheese".split("bread");
-		part1split = Arrays.toString(part1split).split("mayo");
+		//part 1 starts
+		/*/
+		String sandwich1 = "applespineapplesbreadlettucetomatobaconmayohambreadcheese"; 
+		String[] part1split = sandwich1.split("bread");
+		String sandwich2 = Arrays.toString(part1split);
+		System.out.println(sandwich2);
 		
 			//concatenate array into a string
 			//only in between 2 breads
