f391ddafd0ec6f155d5b5e16d8b3cc42f33642f0
APCS3rd <APCS3rd@MHS-LT9-31>
2019-10-28 10:57:32 -0700

    what is the asterisk on the file aaaaaa

--- ArraysLab (the third one)/src/ArraysLab3.java
+++ ArraysLab (the third one)/src/ArraysLab3.java
@@ -7,7 +7,8 @@ public class ArraysLab3 {
 		int[] arr2 = {8, 6, 5};
 		sum(arr1, arr2);
 		append(arr1, 2);
-		
+		remove(arr1, 0);
+		System.out.println(sumEven(arr1));
 		
 		
 
@@ -27,22 +28,26 @@ public class ArraysLab3 {
 				arr2[i]=arr1[i];
 			}
 			arr2[arr1.length]=num;
-			for (int i=0; i<arr1.length+1; i++) {
-				System.out.println(arr2[i]);
-			}
 			return arr2;
 		}
-		public static int[] remove(int[] arr1, int num) {
-			int[] arr2 = new int[arr1.length+1];
+		public static int[] remove(int[] arr1, int idx) {
+			int[] arr2 = new int[arr1.length-1];
+			int idxArr2=0;
 			for (int i=0; i<arr1.length; i++) {
-				arr2[i]=arr1[i];
+				if (i!=idx) {
+					arr2[idxArr2]=arr1[i];
+					idxArr2++;
+				}
 			}
-			arr2[arr1.length]=num;
-			for (int i=0; i<arr1.length+1; i++) {
-				System.out.println(arr2[i]);
-			}
-			
 			return arr2;
+			}
+		public static int sumEven(int[] arr) {
+		int sum=0;
+		for (int i=0;i<arr.length;i++) {
+			if (i%2==0) {
+				sum+=arr[i];
+			}
 		}
-		
+		return sum;
+		}
 }
