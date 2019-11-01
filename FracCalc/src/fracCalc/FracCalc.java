de756f24060073f264c4062b217858e46111ff6b
APCS3rd <APCS3rd@MHS-LT9-31>
2019-11-01 10:22:55 -0700

    it seems to have passed checkpoint one

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -8,14 +8,16 @@ public class FracCalc {
     {
         // TODO: Read the input from the user and call produceAnswer with an equation
     	// TODO: initiate scanner, do basic method that shows that it took input from the scanner, close the loop
+    	// TODO: split the input into 2 factors and the operator
     	Scanner sc = new Scanner(System.in);
     	boolean done = false;
     	while (done==false) {
     		System.out.print("Input please ");
-        	System.out.println(produceAnswer(sc.next()));
+        	System.out.println(produceAnswer(sc.nextLine()));
         	System.out.println("Do you want to keep going? (type \"quit\" to end)");
-			if (sc.next().equals("quit")) done = true;
+			if (sc.nextLine().equals("quit")) done = true;
     	}
+    	sc.close();
     	System.out.println("main method done.");
     }
     
@@ -30,8 +32,11 @@ public class FracCalc {
     public static String produceAnswer(String input)
     { 
         // TODO: Implement this function to produce the solution to the input
-    	
-        return ("your input is "+input);
+    	String[] pt1split = input.split(" ");
+    	String factor1 = pt1split[0];
+    	String operator = pt1split[1];
+    	String factor2 = pt1split[2];
+        return (factor2);
     }
 
     // TODO: Fill in the space below with any helper methods that you think you will need
