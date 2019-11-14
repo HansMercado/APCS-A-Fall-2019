4724a94995a2be39371cf237b560b798888cb99c
APCS3rd <APCS3rd@MHS-LT9-31>
2019-11-14 10:57:06 -0800

    sigh sigh sigh

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -53,9 +53,42 @@ public class FracCalc {
     	else {
     		factor2Arr[0] = part2split[0];
     	}
-        return ("whole:"+factor2Arr[0] +" numerator:"+factor2Arr[1]+" denominator:"+factor2Arr[2]);
+    	///first operand
+    	String[] part3split = factor1.split("_");
+    	String[] factor1Arr = {"0", "0", "1"};
+    	String fraction1 = "";
+    	if (factor1.contains("_")) {
+    		factor1Arr[0] = part3split[0];
+    		fraction1 = part3split[1];
+    	} else fraction1 = factor1;
+    	
+    	if (factor1.contains("/")) {
+    		String [] fraction1Arr = fraction1.split("/");
+    		factor1Arr[1] = fraction1Arr[0];
+    		factor1Arr[2] = fraction1Arr[1];
+    	}
+    	else {
+    		factor1Arr[0] = part3split[0];
+    	}
+    	//part 3 begins
+    	String ans = "";
+    	int[] factor1Ints = {Integer.parseInt(factor1Arr[0]), Integer.parseInt(factor1Arr[1]), Integer.parseInt(factor1Arr[2])};
+    	int[] factor2Ints = {Integer.parseInt(factor2Arr[0]), Integer.parseInt(factor2Arr[1]), Integer.parseInt(factor2Arr[2])};
+    	int[] ansInts = {0, 0, factor1Ints[2]};
+    	factor1 = toImproperFrac(factor1Ints[0], factor1Ints[1], factor1Ints[2]);
+    	factor2 = toImproperFrac(factor2Ints[0], factor2Ints[1], factor2Ints[2]);
+    	if (input.contains("+") || input.contains("-")) {
+    		factor2Ints[1] *= factor1Ints[2];
+    		factor2Ints[2] *= factor1Ints[2];
+    		//AAAAAAAAAAAAAAAAAAAAAAAAAAA COMMON FACTOR OR JUST TIMES THEM
+    		ansInts[0] = factor1Ints[0] + factor2Ints[0];
+    		ansInts[1] = factor1Ints[1] + factor2Ints[1];
+    	}
+        return ("whole:"+ansInts[0] +" numerator:"+ansInts[1]+" denominator:"+ansInts[2]);
     }
 
     // TODO: Fill in the space below with any helper methods that you think you will need
-    
+    public static String toImproperFrac(int whole, int numerator, int denominator) {
+		return (whole*denominator+numerator+"/"+denominator); //gets the whole number and multiplies it by the denominator, then adds it to the numerator. then adds the "/" and the denominator.
+	}
 }
