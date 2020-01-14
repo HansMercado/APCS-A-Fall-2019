1c9c9493f77343652314883247b7b8c2b6f2fe5b
APCS3rd <APCS3rd@MHS-LT9-31>
2020-01-14 09:53:06 -0800

    okay?

---
+++ FracCalc/FracCalc/src/fracCalc/FracCalc.java
@@ -0,0 +1,15 @@
+/*/Hans Mercado
+ * Making FracCalc OOP, accepts expressions and prints out the answer to answer
+ * to the fractional arithmetic problem
+ * START: January 13th 2020
+ */
+package fracCalc;
+
+public class FracCalc {
+    public static void main(String[] args) {
+
+    }
+    //public class fraction
+}
+
+public class
